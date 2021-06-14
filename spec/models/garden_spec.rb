8c296fa00048cdd53e2a9106bd955d326b6bd8e8
Joe Ray <jray100.jr@gmail.com>
2021-06-14 09:26:00 -0600

    Add/update models and their tests

--- spec/models/garden_spec.rb
+++ spec/models/garden_spec.rb
@@ -3,5 +3,21 @@ require 'rails_helper'
 RSpec.describe Garden do
   describe 'relationships' do
     it { should have_many(:plots) }
+    it { should have_many(:plot_plants).through(:plots) }
+    it { should have_many(:plants).through(:plot_plants) }
   end
+
+  # before :each do
+  #
+  # end
+  #
+  # describe 'class methods' do
+  #   describe '.' do
+  #   end
+  # end
+  #
+  # describe 'instance methods' do
+  #   describe '#' do
+  #   end
+  # end
 end
--- spec/models/plot_plant_spec.rb
+++ spec/models/plot_plant_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Plot do
+RSpec.describe PlotPlant, type: :model do
   describe 'relationships' do
-    it { should belong_to(:garden) }
-    it { should have_many(:plot_plants) }
-    it { should have_many(:plants).through(:plot_plants) }
+    it { should belong_to(:plot) }
+    it { should belong_to(:plant) }
   end
 
   # before :each do
